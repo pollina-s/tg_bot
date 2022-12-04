2e7165e24b57a6a59b63e3a565e608a375067b28
Polina Solnishkina <softypie@mail.ru>
2022-12-05 00:07:21 +0300

    Adding Logic

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+import requests
+from pprint import pprint
+from config import tg_token
 
+def get_weather(city, tg_token):
+    try:  #  делаем get-запрос, где получаем и обрабатываем данные
+        r = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={tg_token}&units=metric"
+        )
+        data = r.json()
+        pprint(data)
+    except Exception as ex:  #  сообщение об ошибке
+        print(ex)  #  выводим саму ошибку
+        print("Пожалуйста, проверьте правильность написания города!")
 
+def main():  # запрос города у пользователя
+    city = input("Введите название города: ")
+    get_weather(city, tg_token)
+
+if __name__ == '__main__':
+    main()
