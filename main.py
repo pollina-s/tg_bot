ae0816103f54a765b22f5f876a480e22de230c5b
Polina Solnishkina <softypie@mail.ru>
2022-12-06 00:30:50 +0300

    Adding Logic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from pprint import pprint
+import datetime
 from config import tg_token
 
 def get_weather(city, tg_token):
@@ -8,9 +8,23 @@ def get_weather(city, tg_token):
             f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={tg_token}&units=metric"
         )
         data = r.json()
-        pprint(data)
+
+        city = data["name"]
+        current_weather = data["main"]["temp"]
+        humidity = data["main"]["humidity"]  # влажность
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+
+        sunrise = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        print(f"Погода в городе: {city}\nТемпература: {current_weather}°C\n" # \n - символ новой строки
+              f"Влажность: {humidity}%\nДавление: {pressure} мм рт.ст\n"
+              f"Скорость ветра: {wind} м/с\nРассвет: {sunrise}\n"
+              f"Закат: {sunset}\n"
+              f"Хорошего дня! :)"
+              )
+
     except Exception as ex:  #  сообщение об ошибке
-        print(ex)  #  выводим саму ошибку
         print("Пожалуйста, проверьте правильность написания города!")
 
 def main():  # запрос города у пользователя
